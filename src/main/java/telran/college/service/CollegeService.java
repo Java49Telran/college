f842104ebbcc1b3bce371b148be2e30925eb25e7
Yuragranovsky <yuragranovsky@gmail.com>
2023-12-09 14:20:57 +0200

    HW #52 SQL queries for CollecgeService

--- src/main/java/telran/college/service/CollegeService.java
+++ src/main/java/telran/college/service/CollegeService.java
@@ -2,10 +2,15 @@ package telran.college.service;
 
 import java.util.List;
 
-import telran.college.dto.StudentMark;
+import telran.college.dto.*;
 
 public interface CollegeService {
 	List<String> bestStudentsSubjectType(String type, int nStudents);
-
-	List<StudentMark> studentsAvgMarks();
+	List<NameScore> studentsAvgMarks();
+	List<LecturerHours> lecturersMostHours(int nLecturers);
+	List<StudentCity> studentsScoresLess(int nThreshold);
+	List<NamePhone> studentsBurnMonth(int month);
+	List<NamePhone> lecturersCity(String city);
+	List<NameScore> subjectsScores(String studentName);
+	
 }
